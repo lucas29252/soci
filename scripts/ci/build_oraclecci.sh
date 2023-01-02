58adb5a54d27e48ade7689b15ba1603e599e0c42
Lucas Ayala <lucas.ayala@masmovil.com>
2023-01-02 15:14:29 +0100

    Add dependency with Oracle in tests

--- scripts/ci/build_oraclecci.sh
+++ scripts/ci/build_oraclecci.sh
@@ -7,6 +7,7 @@ source ${SOCI_SOURCE_DIR}/scripts/ci/common.sh
 
 cmake ${SOCI_DEFAULT_CMAKE_OPTIONS} \
     -DWITH_BOOST=OFF \
+    -DSOCI_ORACLE=ON \
     -DSOCI_ORACLECCI=ON \
     -DSOCI_ORACLECCI_TEST_CONNSTR:STRING="service=localhost/XE user=travis password=travis" \
     ..
